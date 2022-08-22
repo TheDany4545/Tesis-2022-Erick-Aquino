c8167e477b0dd4ff7aca67ee25022760bac22603
ErickAquino <erickdanielaquino4545@gmail.com>
2022-08-22 07:52:28 -0600

    Envio de datos linea por linea

--- Programacion/Python_ECG/Serial_data.py
+++ Programacion/Python_ECG/Serial_data.py
@@ -8,12 +8,24 @@ Created on Sun Aug 14 19:41:25 2022
 
 import serial
 import csv
+import pandas as pd
 
 path= r'Z:\Universidad UVG\Sexto Año\Segundo Ciclo\Tesis\Tesis-2022-Erick-Aquino\Programacion\Python_ECG\data2.csv'
-with open(path, "r") as my_file:
-    # pass the file object to reader()
-    file_reader = csv.reader(my_file)
-    # do this for all the rows
-    for i in file_reader:
-        # print the rows
-        print(i)
+ser = serial.Serial(port=('COM4'),baudrate = 115200)
+get_data = pd.read_csv(path,header = None) # lee csv
+get_data[0] = get_data[0].str.replace("'","") # Remove quotes 
+get_data[0] = get_data[0].str.replace(":"," ") #add space
+get_data[0] = get_data[0].str.replace("."," ",regex = False) #add space
+
+################### Enviar linea por linea a traves del serial#################
+d = len(get_data) #Cantidad de datos leido
+i = 0
+for i in range(d):
+    line = get_data.loc[i]
+    send_line = line.to_string(index = False)
+    ser.write(str.encode(send_line))
+    print(send_line)
+    
+
+get_data.to_csv('DATOS_NUEVOS.csv', index = False)
+ser.close()
